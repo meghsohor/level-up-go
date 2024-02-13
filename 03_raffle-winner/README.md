ecf45392a49db694dafae655798d9ab551e264a4
Meghsohor <meghsohor@yahoo.com>
2024-02-13 11:13:06 +0100

    Challenge#3: solution added

---
+++ 03_raffle-winner/README.md
@@ -0,0 +1,9 @@
+# 3. Raffle Winner
+
+In this challenge, we will need to read a list of entries from the `entries.json` file. We will then need to randomly select a winner from the list of entries.
+
+### The challenge requires:
+
+- Reading the `entries.json` file using `os` package.
+- Parsing the JSON data using `encoding/json` package to extract the list of entries as `struct`.
+- Implementing a function to randomly select a winner from the list of entries using `math/rand` package.
---
+++ 03_raffle-winner/my_solution.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"encoding/json"
+	"log"
+	"math/rand"
+	"os"
+	"time"
+)
+
+const path = "entries.json"
+
+// raffleEntry is the struct we unmarshal raffle entries into
+type raffleEntry struct {
+	ID   string `json:"id"`
+	NAME string `json:"name"`
+}
+
+// importData reads the raffle entries from file and creates the entries slice.
+func importData() []raffleEntry {
+	file, err := os.ReadFile(path)
+	if err != nil {
+		log.Fatal(err)
+	}
+	var entries []raffleEntry
+
+	err = json.Unmarshal(file, &entries)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return entries
+}
+
+// getWinner returns a random winner from a slice of raffle entries.
+func getWinner(entries []raffleEntry) raffleEntry {
+	source := rand.NewSource(time.Now().Unix())
+	r := rand.New(source)
+	winner := r.Intn(len(entries))
+
+	return entries[winner]
+}
+
+func main() {
+	entries := importData()
+	log.Println("And... the raffle winning entry is...")
+	winner := getWinner(entries)
+	time.Sleep(500 * time.Millisecond)
+	log.Println(winner)
+}
